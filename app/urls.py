e3340187cb53323c74d2db04722d05450467e466
Luis Ore Cardenas <luisore24@gmail.com>
2021-08-02 20:40:44 -0500

    add SesionCursoViewSet

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework import urlpatterns
 from rest_framework.routers import DefaultRouter
-from .views import BeneficioViewSet, CategoriaViewSet, CursoViewSet, HorarioCursoViewSet, HorarioViewSet, SemanaViewSet, SesionViewSet
+from .views import BeneficioViewSet, CategoriaViewSet, CursoViewSet, HorarioCursoViewSet, HorarioViewSet, SemanaViewSet, SesionCursoViewSet, SesionViewSet
 
 router = DefaultRouter()
 router.register('categorias', CategoriaViewSet)
 router.register('cursos', CursoViewSet)
 router.register('sesiones', SesionViewSet)
+router.register('sesiones-cursos', SesionCursoViewSet)
 router.register('semanas', SemanaViewSet)
 router.register('horarios', HorarioViewSet)
 router.register('horario-cursos', HorarioCursoViewSet)
